7730a074190a96600a42ae6a55fe9852e466507a
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-06-26 20:25:57 +0300

    Phishing Simulation and Security Awareness

--- handlers/campaign.go
+++ handlers/campaign.go
@@ -1,1 +1,42 @@
 package handlers
+
+import (
+	"github.com/c9b3rd3vi1/NetGuardIQ/database"
+	"github.com/c9b3rd3vi1/NetGuardIQ/models"
+	fiber "github.com/gofiber/fiber/v2"
+)
+
+// Create Campaign Dashboard
+
+func Dashboard(c *fiber.Ctx) error {
+	var campaigns []models.Campaign
+	if err := database.DB.Preload("Target").Find(&campaigns).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString("Error fetching campaigns")
+	}
+	return c.Render("dashboard", fiber.Map{
+		//"Title":     "Dashboard",
+		"Campaigns": campaigns,
+	})
+}
+
+// Create New Campaign Form
+func NewCampaignForm(c *fiber.Ctx) error {
+	return c.Render("create_campaign", fiber.Map{
+		//"Title": "New Campaign",
+	})
+}
+
+// Create Campaign in Database
+func CreateCampaign(c *fiber.Ctx) error {
+	name := c.FormValue("name")
+	description := c.FormValue("description")
+	campaign := models.Campaign{
+		Name:        name,
+		Description: description,
+	}
+
+	if err := database.DB.Create(&campaign).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString("Error creating campaign")
+	}
+	return c.Redirect("/campaigns/new")
+}
--- models/models.go
+++ models/models.go
@@ -1,1 +1,24 @@
 package models
+
+import "gorm.io/gorm"
+
+
+// campaign model represents a phishing campaign
+type Campaign struct {
+	gorm.Model
+	Name        string `gorm:"not null"`
+	Description string `gorm:"not null"`
+	Target []Target
+}
+
+
+// Target users or victims of the phishing campaign
+type Target struct {
+	gorm.Model
+	Name	 string `gorm:"not null"`
+	Email	 string `gorm:"not null"`
+	Phone	 string `gorm:"not null"`
+	Clicked   bool   `gorm:"default:false"`
+	Submitted bool   `gorm:"default:false"`
+	CampaignID uint
+}
